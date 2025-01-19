e92f2bcc0324d0f8f295312153edce2970ee31d2
teniolafatunmbi <fatunmbiteniola001@gmail.com>
2025-01-19 21:28:38 +0100

    docs(products): Add docs for products endpoints

---
+++ docs/products/list-all-products.md
@@ -0,0 +1,92 @@
+---
+id: list-all-products
+title: List all products
+sidebar_label: List all products
+---
+
+# List products
+
+This endpoint fetches all products in an organization
+
+### Authorization
+
+`Authorization` `string` `header` `required`
+
+## Endpoint
+
+`GET` /products
+
+### Query Parameters
+
+| Parameter         | Type   | Required | Description                                                                                                                                      |
+| ----------------- | ------ | -------- | ------------------------------------------------------------------------------------------------------------------------------------------------ |
+| `organization_id` | string | Yes      | The ID of the organization the product belongs to                                                                                                |
+| `supplier_id`     | string | No       | The ID of the supplier that supplied stocks of a product                                                                                         |
+| `category_id`     | string | No       | The ID of the category to filter a product by                                                                                                    |
+| `search_value`    | string | No       | A search value to filter products name by                                                                                                        |
+| `sorting_key`     | string | No       | The field to sort the products by. It must be one of the sortable fields on the product object. For example, date_created, name, or last_updated |
+| `page`            | int    | No       | Page to fetch data from. Default 1                                                                                                               |
+| `size`            | int    | No       | Size of the response items. Default 50. Max 100                                                                                                  |
+| `currency_code`   | string | No       | The currency code of the products to retrieve                                                                                                    |
+| `reverse_sort`    | bool   | No       | Sorting order of the retrieved items. Default to true (desc)                                                                                     |
+| `debug`           | bool   | No       | Returns debug info. Defaults to false.                                                                                                           |
+
+## Example Request
+
+```bash
+curl -X GET "https://api.timbu.cloud/products?organization_id=799bbdca76254f5c83f1d0f35cfb7e30"
+```
+
+## Example Response
+
+```bash
+{
+    "page": 1,
+    "size": 50,
+    "total": 2,
+    "debug": null,
+    "previous_page": null,
+    "next_page": null,
+    "items": [
+        {
+            "name": "test product",
+            "description": null,
+            "unique_id": "TE964P",
+            "url_slug": "test-product",
+            "is_available": true,
+            "is_service": false,
+            "previous_url_slugs": null,
+            "unavailable": false,
+            "unavailable_start": null,
+            "unavailable_end": null,
+            "id": "0529002da1b24bc79432071cf412ec13",
+            "parent_product_id": null,
+            "parent": null,
+            "organization_id": "799bbdca76254f5c83f1d0f35cfb7e30",
+            "categories": [],
+            "date_created": "2025-01-19T20:08:22",
+            "last_updated": "2025-01-19T20:08:22",
+            "user_id": "e780b95df89041e8b93831e16dac5e52",
+            "current_price": [
+                {}
+            ],
+            "is_deleted": false,
+            "available_quantity": 0.0,
+            "selling_price": null,
+            "discounted_price": null,
+            "buying_price": null,
+            "photos": [],
+        }
+    ]
+}
+```
+
+### Response Codes
+
+| Code  | Description                                            |
+| ----- | ------------------------------------------------------ |
+| `200` | OK. Request was successful                             |
+| `400` | Bad Request. The request was invalid.                  |
+| `404` | Not Found. The resource was not found.                 |
+| `422` | Validation error.                                      |
+| `500` | Internal Server Error. An error occurred on the server |
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,132 +1,132 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-	title: "Timbu API Documentation",
-	tagline: "Come and learn the timbu way",
-	favicon: "img/favicon.ico",
+  title: "Timbu API Documentation",
+  tagline: "Build E-commerce platforms with a ready-to-use backend",
+  favicon: "img/favicon.ico",
 
-	// Set the production url of your site here
-	url: "https://docs.timbu.cloud",
-	// Set the /<baseUrl>/ pathname under which your site is served
-	// For GitHub pages deployment, it is often '/<projectName>/'
-	baseUrl: "/",
+  // Set the production url of your site here
+  url: "https://docs.timbu.cloud",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
 
-	// GitHub pages deployment config.
-	// If you aren't using GitHub pages, you don't need these.
-	organizationName: "Timbu", // Usually your GitHub org/user name.
-	projectName: "Timbu API documentation", // Usually your repo name.
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "Timbu", // Usually your GitHub org/user name.
+  projectName: "Timbu API documentation", // Usually your repo name.
 
-	onBrokenLinks: "throw",
-	onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
-	// Even if you don't use internationalization, you can use this field to set
-	// useful metadata like html lang. For example, if your site is Chinese, you
-	// may want to replace "en" with "zh-Hans".
-	i18n: {
-		defaultLocale: "en",
-		locales: ["en"],
-	},
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
 
-	presets: [
-		[
-			"classic",
-			{
-				docs: {
-					sidebarPath: "./sidebars.ts",
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					routeBasePath: "/api",
-					editUrl: "https://github.com/timbuapp/docs.git",
-				},
-				blog: {
-					showReadingTime: true,
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					editUrl: "https://github.com/timbuapp/docs.git",
-				},
-				theme: {
-					customCss: require.resolve("./src/css/custom.css"),
-				},
-			} satisfies Preset.Options,
-		],
-	],
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          routeBasePath: "/api",
+          editUrl: "https://github.com/timbuapp/docs.git",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/timbuapp/docs.git",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
 
-	themeConfig: {
-		// Replace with your project's social card
-		image: "img/docusaurus-social-card.jpg",
-		navbar: {
-			title: "",
-			logo: {
-				alt: "Timbu Logo",
-				src: "img/logo.svg",
-			},
-			items: [
-				{
-					type: "docSidebar",
-					sidebarId: "tutorialSidebar",
-					position: "left",
-					label: "Tutorial",
-				},
-				{ to: "/blog", label: "Blog", position: "left" },
-				{
-					href: "https://github.com/timbuapp/docs",
-					label: "GitHub",
-					position: "right",
-				},
-			],
-		},
-		footer: {
-			style: "dark",
-			links: [
-				{
-					title: "Docs",
-					items: [
-						{
-							label: "Tutorial",
-							to: "/api/intro",
-						},
-					],
-				},
-				{
-					title: "Community",
-					items: [
-						{
-							label: "Stack Overflow",
-							href: "https://stackoverflow.com/questions/tagged/docusaurus",
-						},
-						{
-							label: "Discord",
-							href: "https://discordapp.com/invite/docusaurus",
-						},
-						{
-							label: "Twitter",
-							href: "https://twitter.com/docusaurus",
-						},
-					],
-				},
-				{
-					title: "More",
-					items: [
-						{
-							label: "Blog",
-							to: "/blog",
-						},
-						{
-							label: "GitHub",
-							href: "https://github.com/timbuapp",
-						},
-					],
-				},
-			],
-			copyright: `Copyright © ${new Date().getFullYear()} Timbu`,
-		},
-		prism: {
-			theme: prismThemes.github,
-			darkTheme: prismThemes.dracula,
-		},
-	} satisfies Preset.ThemeConfig,
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "Timbu",
+      logo: {
+        alt: "Timbu Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "docsSidebar",
+          position: "left",
+          label: "Tutorial",
+        },
+        { to: "/blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/timbuapp",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Tutorial",
+              to: "/api/intro",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "/blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/timbuapp",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Timbu`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
 };
 
 export default config;
